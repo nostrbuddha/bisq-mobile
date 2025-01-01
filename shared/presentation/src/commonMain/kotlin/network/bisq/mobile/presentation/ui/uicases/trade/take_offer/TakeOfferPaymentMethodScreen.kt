624e290d4fb9ca024ef2876b0ed943778a6466c5
nostrbuddha <nostrbuddha@proton.me>
2025-01-01 22:40:11 +0530

    - Fixed compile time issues

--- shared/presentation/src/commonMain/kotlin/network/bisq/mobile/presentation/ui/uicases/trade/take_offer/TakeOfferPaymentMethodScreen.kt
+++ shared/presentation/src/commonMain/kotlin/network/bisq/mobile/presentation/ui/uicases/trade/take_offer/TakeOfferPaymentMethodScreen.kt
@@ -1,36 +1,15 @@
 package network.bisq.mobile.presentation.ui.uicases.trade.take_offer
 
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.unit.dp
 import cafe.adriel.lyricist.LocalStrings
-import kotlinx.coroutines.flow.MutableStateFlow
-import network.bisq.mobile.i18n.toDisplayString
 import network.bisq.mobile.presentation.ui.components.atoms.BisqText
-import network.bisq.mobile.presentation.ui.components.atoms.DynamicImage
-import network.bisq.mobile.presentation.ui.components.atoms.PaymentTypeCard
 import network.bisq.mobile.presentation.ui.components.atoms.layout.BisqGap
 import network.bisq.mobile.presentation.ui.components.layout.MultiScreenWizardScaffold
-import network.bisq.mobile.presentation.ui.components.molecules.BisqPaymentToggle
 import network.bisq.mobile.presentation.ui.components.organisms.PaymentMethodCard
 import network.bisq.mobile.presentation.ui.helpers.RememberPresenterLifecycle
 import network.bisq.mobile.presentation.ui.theme.BisqTheme
-import network.bisq.mobile.presentation.ui.theme.BisqUIConstants
 import org.koin.compose.koinInject
 
 @Composable
